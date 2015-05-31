fa31445600ac51dce4d6aecfa17fbe3446d4b779
hustlzp <hustlzp@qq.com>
2015-05-31 12:35:33 +0800

    Rm useless validator in AdminTopicForm.

--- application/forms/topic.py
+++ application/forms/topic.py
@@ -9,11 +9,6 @@ class AdminTopicForm(Form):
     name = StringField('话题名称', validators=[DataRequired('话题名称不能为空')])
     desc = TextAreaField('话题描述')
 
-    def validate_name(self, field):
-        topic = Topic.get_by_name(field.data)
-        if topic:
-            raise ValueError('话题名称已存在')
-
 
 class EditTopicWikiForm(Form):
     wiki = TextAreaField('Wiki', validators=[])
