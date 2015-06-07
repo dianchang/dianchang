e70b752c6570162dcfc908ab2a7bf45d9f2f9cca
hustlzp <hustlzp@qq.com>
2015-06-07 11:28:17 +0800

    Update style for signin page.

---
+++ application/static/js/account/signin.js
@@ -0,0 +1,9 @@
+$('.btn-go-to-signup').click(function () {
+    $('.signup-wap').show();
+    $('.signin-wap').hide();
+});
+
+$('.btn-go-to-signin').click(function () {
+    $('.signin-wap').show();
+    $('.signup-wap').hide();
+});
