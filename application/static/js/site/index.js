a7d3c943e077113a25c3e036a52f5f2dc2ee1d1b
hustlzp <hustlzp@qq.com>
2015-06-08 15:52:51 +0800

    Finish the index page slide effect.

--- application/static/js/site/index.js
+++ application/static/js/site/index.js
@@ -2,6 +2,15 @@ $accountWap = $('.dc-signin-signup-forgot-pwd-wap');
 
 // 切换到登录框
 $('.btn-go-to-signin-signup').click(function () {
-    $('.logo-wap').hide();
-    $accountWap.show();
+    $accountWap.css({
+        'margin-left': '-280px'
+    }).show().animate({
+        'margin-left': '0'
+    }, 180, function () {
+        $(this).css({
+            position: 'static',
+            margin: '50px 0 0 0'
+        });
+        $('.logo-wap').hide();
+    });
 });
