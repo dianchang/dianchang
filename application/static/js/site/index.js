8697ab7054a5555c0d521fd82e55d96f4f15d966
hustlzp <hustlzp@qq.com>
2015-06-08 16:24:40 +0800

    Finish slide effect of signin & signup & forgot pwd waps.

--- application/static/js/site/index.js
+++ application/static/js/site/index.js
@@ -3,13 +3,13 @@ $accountWap = $('.dc-signin-signup-forgot-pwd-wap');
 // 切换到登录框
 $('.btn-go-to-signin-signup').click(function () {
     $accountWap.css({
-        'margin-left': '-280px'
+        'left': '-280px'
     }).show().animate({
-        'margin-left': '0'
+        'left': '0'
     }, 180, function () {
         $(this).css({
-            position: 'static',
-            margin: '50px 0 0 0'
+            position: 'relative',
+            margin: '0'
         });
         $('.logo-wap').hide();
     });
