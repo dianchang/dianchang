8f4085e8504622bdade09f273ed7ded466ab363c
hustlzp <hustlzp@qq.com>
2015-06-10 11:09:15 +0800

    Add preview to answer.view page.

--- application/static/js/answer/view.js
+++ application/static/js/answer/view.js
@@ -134,3 +134,10 @@ $(document).onOnce('click', '.btn-submit-comment', function () {
         }
     });
 });
+
+// 继续阅读
+$('.btn-continue-reading').click(function () {
+    var $answer = $(this).parents('.more-answer');
+
+    $answer.addClass('full');
+});
