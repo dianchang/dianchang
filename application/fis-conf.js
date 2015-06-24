a149cfe7c810e5efc212439600acab7fbb30d989
hustlzp <hustlzp@qq.com>
2015-06-24 13:07:18 +0800

    Use fis to package static files.

--- application/fis-conf.js
+++ application/fis-conf.js
@@ -1,3 +1,18 @@
 fis.config.set('project.include', ['templates/**', 'static/**']);
 fis.config.set('project.exclude', ['static/bower_components/**', 'static/**.less']);
-fis.config.set('project.fileType.text', 'otf');
+fis.config.set('modules.postpackager', 'simple');
+fis.config.set('pack', {
+    '/pkg/lib.js': [
+        'static/js/libs/*.js',
+        'static/js/init.js'
+    ],
+    '/pkg/lib.css': [
+        'static/css/libs/*.css'
+    ],
+    '/pkg/layout.css': [
+        'static/css/bootstrap.theme.css',
+        'static/css/common.css',
+        'static/css/macros/*.css',
+        'static/css/layout.css'
+    ]
+});
