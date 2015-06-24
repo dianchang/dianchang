03df6f90edf85f1c45b30834ed44de2f3eebaeff
hustlzp <hustlzp@qq.com>
2015-06-24 12:56:16 +0800

    Use fis to compile resources.

---
+++ application/fis-conf.js
@@ -0,0 +1,3 @@
+fis.config.set('project.include', ['templates/**', 'static/**']);
+fis.config.set('project.exclude', ['static/bower_components/**', 'static/**.less']);
+fis.config.set('project.fileType.text', 'otf');
