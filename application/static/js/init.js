0d13134a023887c51dfcbcd0ce5b38d619f17a72
hustlzp <hustlzp@qq.com>
2015-07-07 11:50:03 +0800

    Update windown.disableScroll & window.enableScroll functions.

--- application/static/js/init.js
+++ application/static/js/init.js
@@ -198,34 +198,18 @@
         return str.slice(-suffix.length) === suffix;
     }
 
-    function preventDefault(e) {
-        e = e || window.event;
-        if (e.preventDefault)
-            e.preventDefault();
-        e.returnValue = false;
-    }
-
     /**
      * 禁止窗口滚动
      */
     function disableScroll() {
-        if (window.addEventListener) // older FF
-            window.addEventListener('DOMMouseScroll', preventDefault, false);
-        window.onwheel = preventDefault; // modern standard
-        window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
-        window.ontouchmove = preventDefault; // mobile
+        $('body').addClass('modal-open');
     }
 
     /**
      * 使能窗口滚动
      */
     function enableScroll() {
-        if (window.removeEventListener)
-            window.removeEventListener('DOMMouseScroll', preventDefault, false);
-        window.onmousewheel = document.onmousewheel = null;
-        window.onwheel = null;
-        window.ontouchmove = null;
-        document.onkeydown = null;
+        $('body').removeClass('modal-open');
     }
 
     function getJsonFromUrl() {
