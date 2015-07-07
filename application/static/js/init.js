c25ee72e31a98292b6704f536642913ee0f374f6
hustlzp <hustlzp@qq.com>
2015-07-07 10:45:50 +0800

    Disable window scroll when open follow-bg.

--- application/static/js/init.js
+++ application/static/js/init.js
@@ -198,10 +198,36 @@
         return str.slice(-suffix.length) === suffix;
     }
 
+    function preventDefault(e) {
+        e = e || window.event;
+        if (e.preventDefault)
+            e.preventDefault();
+        e.returnValue = false;
+    }
+
+    function disableScroll() {
+        if (window.addEventListener) // older FF
+            window.addEventListener('DOMMouseScroll', preventDefault, false);
+        window.onwheel = preventDefault; // modern standard
+        window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+        window.ontouchmove = preventDefault; // mobile
+    }
+
+    function enableScroll() {
+        if (window.removeEventListener)
+            window.removeEventListener('DOMMouseScroll', preventDefault, false);
+        window.onmousewheel = document.onmousewheel = null;
+        window.onwheel = null;
+        window.ontouchmove = null;
+        document.onkeydown = null;
+    }
+
     window.showTip = showTip;
     window.hideTip = hideTip;
     window.urlFor = urlFor;
     window.registerContext = registerContext;
     window.startsWith = startsWith;
     window.endsWith = endsWith;
+    window.disableScroll = disableScroll;
+    window.enableScroll = enableScroll;
 })();
