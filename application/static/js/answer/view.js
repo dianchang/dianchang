bf55bbd600444fb90bb577b93157bbe2c8a5b87e
hustlzp <hustlzp@qq.com>
2015-07-10 13:32:11 +0800

    Add redirect effect to more answer.

--- application/static/js/answer/view.js
+++ application/static/js/answer/view.js
@@ -6,21 +6,32 @@ $('.btn-continue-reading').click(function () {
 });
 
 // 高亮评论
-var hash = window.location.hash;
-if (startsWith(hash, '#comment')) {
-    var commentId = hash.split('#comment')[1];
-    var $comment = $(".answer-comment[data-id='" + commentId + "']");
+//var hash = window.location.hash;
+//if (startsWith(hash, '#comment')) {
+//    var commentId = hash.split('#comment')[1];
+//    var $comment = $(".answer-comment[data-id='" + commentId + "']");
+//
+//    $comment.css('backgroundColor', '#E6F9F2');
+//    $(window).scrollTo($comment, {
+//        offset: {
+//            top: -200
+//        }
+//    });
+//
+//    setTimeout(function () {
+//        $comment.animate({
+//            'backgroundColor': '#f5f5f5'
+//        }, 2000);
+//    });
+//}
 
-    $comment.css('backgroundColor', '#E6F9F2');
-    $(window).scrollTo($comment, {
-        offset: {
-            top: -200
-        }
-    });
+// 跳转回答
+$('.more-answer').click(function (e) {
+    var href = $(this).data('href');
 
-    setTimeout(function () {
-        $comment.animate({
-            'backgroundColor': '#f5f5f5'
-        }, 2000);
-    });
-}
+    if (e.target.targetName === 'A' || $(e.target).parents('a').length !== 0) {
+        return true;
+    }
+
+    window.location = href;
+});
